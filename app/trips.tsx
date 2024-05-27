76e055fa19f0eda267010a660ef63be922c145a5
Arima057nn <tjendung97pain@gmail.com>
2024-05-27 18:04:05 +0700

    add loading + modal yesno

--- app/trips.tsx
+++ app/trips.tsx
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { TripInterface } from "@/interfaces/booking";
 import { bookingApi } from "@/services/booking-api";
@@ -6,10 +12,23 @@ import Colors from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import { BOOKING_STATUS } from "@/constants/Status";
 import { useTrips } from "@/contexts/tripsContext";
+import IsLoadingModal from "@/components/isLoadingModal";
+import Modal from "@/components/modal";
+import { ModalInterface } from "@/interfaces/modal";
+import { defaultStyles } from "@/constants/Styles";
+import AnswerModal from "@/components/answerModal";
 
 const Trips = () => {
+  const [loading, setLoading] = useState(false);
+  const [selectedTrip, setSelectedTrip] = useState<TripInterface | null>(null);
+  const [keepingTrip, setKeepingTrip] = useState<TripInterface | null>(null);
   const [trips, setTrips] = useState<TripInterface[]>([]);
   const { noTrip } = useTrips();
+  const [modalContent, setModalContent] = useState<ModalInterface>({
+    isOpen: false,
+    title: "",
+    description: "",
+  });
 
   const getTripsCurrent = async () => {
     const res = await bookingApi.getTripsCurrent();
@@ -26,47 +45,125 @@ const Trips = () => {
   const handleFinishTrip = async (trip: TripInterface) => {
     console.log("trip", trip.cyclingId.code);
     if (trip.status === BOOKING_STATUS.ACTIVE) {
+      setLoading(true);
       const res = await bookingApi.createTripDetail(
         trip._id,
         "65fbeeed40b7773e46da92c1"
       );
+      setLoading(false);
       if (res.status === 200) {
-        Alert.alert("Kết thúc chuyến đi thành công");
+        setModalContent({
+          isOpen: true,
+          title: "Thành công",
+          description: "Kết thúc chuyến đi thành công",
+        });
         getTripsCurrent();
       } else {
-        Alert.alert("Kết thúc chuyến đi thất bại", res.data.error);
+        setModalContent({
+          isOpen: true,
+          title: "Thất bại",
+          description: res.data.error,
+        });
       }
+    } else {
+      setModalContent({
+        isOpen: true,
+        title: "Thất bại",
+        description: "Chuyến đi không tồn tại hoặc đã kết thúc",
+      });
     }
   };
 
   const handleCancelKeepingTrip = async (trip: TripInterface) => {
     console.log("trip", trip.cyclingId.code);
     if (trip.status === BOOKING_STATUS.KEEPING) {
+      setLoading(true);
       const res = await bookingApi.cancelKeeping(
         trip._id,
         trip.cyclingId.category._id
       );
+      setLoading(false);
       if (res.status === 200) {
-        Alert.alert("Hủy giữ xe thành công");
+        setModalContent({
+          isOpen: true,
+          title: "Thành công",
+          description: res.data.message,
+        });
         getTripsCurrent();
       } else {
-        Alert.alert("Hủy giữ xe thất bại", res.data.error);
+        setModalContent({
+          isOpen: true,
+          title: "Thất bại",
+          description: res.data.error,
+        });
       }
+    } else {
+      setModalContent({
+        isOpen: true,
+        title: "Thất bại",
+        description: "Chuyến đi không tồn tại hoặc đã kết thúc",
+      });
     }
   };
 
   const handleStartFromKeepingTrip = async (trip: TripInterface) => {
     console.log("trip", trip.cyclingId.code);
     if (trip.status === BOOKING_STATUS.KEEPING) {
+      setLoading(true);
       const res = await bookingApi.startFromKeeping(trip._id);
+      setLoading(false);
       if (res.status === 200) {
-        Alert.alert("Bắt đầu chuyến đi thành công");
+        setModalContent({
+          isOpen: true,
+          title: "Thành công",
+          description: "Bắt đầu chuyến đi thành công",
+        });
         getTripsCurrent();
+      } else {
+        setModalContent({
+          isOpen: true,
+          title: "Thất bại",
+          description: res.data.error,
+        });
       }
+    } else {
+      setModalContent({
+        isOpen: true,
+        title: "Thất bại",
+        description: "Chuyến đi không tồn tại hoặc đã kết thúc",
+      });
     }
   };
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" animated={true} />
+      {loading && <IsLoadingModal />}
+      <Modal
+        title={modalContent.title}
+        description={modalContent.description}
+        isOpen={modalContent.isOpen}
+        onRequestClose={() => {
+          setModalContent((prevState) => ({
+            ...prevState,
+            isOpen: false,
+          }));
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            setModalContent((prevState) => ({
+              ...prevState,
+              isOpen: false,
+            }));
+          }}
+          style={{
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
+          <Text style={defaultStyles.textOK}>OK</Text>
+        </TouchableOpacity>
+      </Modal>
       <Text style={styles.title}>Xe đang sử dụng</Text>
 
       {trips.map((trip) => (
@@ -91,13 +188,39 @@ const Trips = () => {
           </View>
           <View>
             {trip.status === BOOKING_STATUS.ACTIVE && (
-              <TouchableOpacity
-                onPress={() => handleFinishTrip(trip)}
-                activeOpacity={0.8}
-                style={styles.actionBtn}
-              >
-                <Ionicons name="stop" size={16} style={styles.icon} />
-              </TouchableOpacity>
+              <View>
+                <TouchableOpacity
+                  onPress={() => setSelectedTrip(trip)}
+                  activeOpacity={0.8}
+                  style={styles.actionBtn}
+                >
+                  <Ionicons name="stop" size={16} style={styles.icon} />
+                </TouchableOpacity>
+                <AnswerModal
+                  title="Chuyến đi"
+                  description="Kết thúc ngay bây giờ?"
+                  isOpen={selectedTrip?._id === trip._id}
+                  onRequestClose={() => setSelectedTrip(null)}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View style={defaultStyles.containerTextOK}>
+                      <TouchableOpacity onPress={() => setSelectedTrip(null)}>
+                        <Text style={{ fontSize: 16 }}>Hủy bỏ</Text>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={defaultStyles.containerTextOK}>
+                      <TouchableOpacity onPress={() => handleFinishTrip(trip)}>
+                        <Text style={defaultStyles.textOK}>Đồng ý</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </AnswerModal>
+              </View>
             )}
             {trip.status === BOOKING_STATUS.KEEPING && (
               <View
@@ -114,7 +237,7 @@ const Trips = () => {
                   <Ionicons name="close" size={16} style={styles.icon} />
                 </TouchableOpacity>
                 <TouchableOpacity
-                  onPress={() => handleStartFromKeepingTrip(trip)}
+                  onPress={() => setKeepingTrip(trip)}
                   activeOpacity={0.8}
                   style={styles.actionBtn}
                 >
@@ -124,6 +247,32 @@ const Trips = () => {
                     style={styles.icon}
                   />
                 </TouchableOpacity>
+                <AnswerModal
+                  title="Chuyến đi"
+                  description="Bắt đầu chuyến đi ngay bây giờ?"
+                  isOpen={keepingTrip?._id === trip._id}
+                  onRequestClose={() => setKeepingTrip(null)}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View style={defaultStyles.containerTextOK}>
+                      <TouchableOpacity onPress={() => setKeepingTrip(null)}>
+                        <Text style={{ fontSize: 16 }}>Hủy bỏ</Text>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={defaultStyles.containerTextOK}>
+                      <TouchableOpacity
+                        onPress={() => handleStartFromKeepingTrip(trip)}
+                      >
+                        <Text style={defaultStyles.textOK}>Đồng ý</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </AnswerModal>
               </View>
             )}
           </View>
